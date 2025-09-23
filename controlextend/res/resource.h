99842fc3853ce04617f47c27945a85f18578ef72
tangaidongd <32316682+tangaidongd@users.noreply.github.com>
2025-09-23 23:20:03 +0800

    Add files via upload

---
+++ controlextend/res/resource.h
@@ -0,0 +1,63 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by controlextend.rc
+//
+#define IDR_PNG_MENU_FOLD_COLOR         6001
+#define IDR_PNG_MENU_EXPAND_COLOR       6002
+#define IDR_PNG_MENU_FOLD_BLACK         6003
+#define IDR_PNG_MENU_EXPAND_BLACK       6004
+#define IDB_BITMAP_ARROW                6005
+#define IDB_HORIZONTAL_SCROLLBAR_LEFTARROW 6006
+#define IDB_HORIZONTAL_SCROLLBAR_RIGHTARROW 6007
+#define IDB_HORIZONTAL_SCROLLBAR_CHANNEL 6008
+#define IDB_HORIZONTAL_SCROLLBAR_THUMB  6009
+#define IDB_HORIZONTAL_SCROLLBAR_THUMB_NO_COLOR 6010
+#define IDB_VERTICAL_SCROLLBAR_DOWNARROW 6011
+#define IDB_VERTICAL_SCROLLBAR_UPARROW  6012
+#define IDB_VERTICAL_SCROLLBAR_CHANNEL  6013
+#define IDB_VERTICAL_SCROLLBAR_THUMB    6014
+#define IDB_VERTICAL_SCROLLBAR_THUMB_NO_COLOR 6015
+#define IDR_PNG_DLG_BTN                 6016
+#define IDR_PNG_DIALOG_CAPTION_BIG      6017
+#define IDB_PNG1                        6018
+#define IDR_PNG_DIALOG_CAPTION_SMALL    6018
+#define IDR_PNG_SPIN_ADD                6019
+#define IDB_PNG3                        6020
+#define IDR_PNG_SPIN_SUB                6020
+#define IDB_GUITAB_NORMAL               6021
+#define IDB_TAB                         6022
+#define IDB_GUITAB_SELECTED             6023
+#define IDR_PNG_MERCH                   6024
+#define IDR_PNG_BLOCK                   6025
+#define IDR_PNG_INDEX                   6026
+#define IDR_PNG_SHORT_CUT               6027
+#define IDR_PNG_WSP                     6028
+#define IDR_PNG_CFM                     6029
+#define IDR_PNG_VMG                     6030
+#define IDB_BITMAP1                     6031
+#define IDB_GUI_DOCKBAR                 6031
+#define IDB_BITMAP_CMTARROW             6032
+#define ID_TAB_BEGIN                    33478
+#define ID_TAB_RENAME                   33478
+#define ID_TAB_ADD                      33479
+#define ID_TAB_DEL                      33480
+#define ID_TAB_STYLE1                   33481
+#define ID_TAB_STYLE2                   33482
+#define ID_TAB_STYLE3                   33483
+#define ID_TAB_STYLE4                   33484
+#define ID_TAB_STYLE5                   33485
+#define ID_TAB_STYLE6                   33486
+#define ID_TAB_STYLE7                   33487
+#define ID_TAB_STYLE8                   33488
+#define ID_TAB_END                      33490
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        6033
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         6000
+#define _APS_NEXT_SYMED_VALUE           6000
+#endif
+#endif
